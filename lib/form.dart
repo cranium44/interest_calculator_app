f4a9f74adc6a9d68e27ce83b53481e28ca0af748
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-17 03:28:23 +0100

    created form class

---
+++ lib/form.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SUIForm extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+
+    return null;
+  }
+}
+
+class _SUIFormState extends State<SUIForm>{
+  String inter
+  @override
+  Widget build(BuildContext context) {
+
+    return null;
+  }
+
+}
