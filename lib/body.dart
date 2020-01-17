3fecad292a9c8738c9a503211b6979f5aa5f8db8
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-17 03:25:44 +0100

    added image to the body

--- lib/body.dart
+++ lib/body.dart
@@ -4,7 +4,23 @@ class Body extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return null;
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          getImageAsset(),
+          SUIForm()
+        ],
+      ),
+    );
+  }
+
+  Image getImageAsset() {
+    var assetImage = new AssetImage("images/download.png");
+    var image = Image(image: assetImage,);
+    return image;
   }
 
 }
+
