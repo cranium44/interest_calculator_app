3c3ceb4bfd9bf93ab177f9767edd39b6b27c409e
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-01-17 03:46:27 +0100

    created the principal text input of form

--- lib/form.dart
+++ lib/form.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 
-class SUIForm extends StatefulWidget{
+class SUIForm extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-
-    return null;
+    return _SUIFormState();
   }
 }
 
-class _SUIFormState extends State<SUIForm>{
-  String inter
+class _SUIFormState extends State<SUIForm> {
+  var _minimumPadding = 5.0;
+
   @override
   Widget build(BuildContext context) {
-
-    return null;
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(_minimumPadding),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Principal",
+                  hintText: "Principal e.g 12000",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(_minimumPadding)
+                  )
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
-
 }
